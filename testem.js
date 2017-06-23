2d4dec8ac5c8bc0c576022b7b7b74c0d88a382fc
snewcomer <snewcomer24@gmail.com>
2017-06-23 10:17:08 -0700

    [EMBER]: 2.13 alternative approach

--- testem.js
+++ testem.js
@@ -1,4 +1,4 @@
-/*jshint node:true*/
+/* eslint-env node */
 module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
@@ -8,5 +8,7 @@ module.exports = {
     "Chrome"
   ],
   "launch_in_dev": [
+    "Firefox",
+    "Chrome"
   ]
 };
